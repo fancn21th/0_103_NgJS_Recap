11f06440eb6d1a78eb171d2eb910ba95f093ac2e
alex.fan <alex.fan@okchem.com>
2019-08-22 09:59:32 +0800

    ui-router in v1.0

--- src/app/app.js
+++ src/app/app.js
@@ -1,11 +1,19 @@
 angular
   .module("recap", ["ui.router", "categories", "categories.bookmarks"])
-  .config(function($stateProvider) {
-    var categoriesState = {
-      url: "/categories",
-      templateUrl: "app/categories/categories.tmpl.html",
-      controller: "MainCtrl"
+  .config(function($stateProvider, $urlRouterProvider) {
+    var helloState = {
+      name: "hello",
+      url: "/hello",
+      template: "<h3>hello world!</h3>"
     };
-    $stateProvider.state("categories", categoriesState);
+
+    var aboutState = {
+      name: "about",
+      url: "/about",
+      template: "<h3>Its the UI-Router hello world app!</h3>"
+    };
+
+    $stateProvider.state(helloState);
+    $stateProvider.state(aboutState);
   })
   .controller("MainCtrl", function($scope) {});
