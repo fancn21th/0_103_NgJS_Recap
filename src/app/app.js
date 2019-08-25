d3b82670349d5fe6bd39c08ff5413e930247301f
fancn <fanyj@yikeer.com>
2019-08-25 09:39:18 +0800

    ngAnimation

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,10 @@
 angular
-  .module("recap", ["ui.router", "categories", "categories.bookmarks"])
+  .module("recap", [
+    "ngAnimate",
+    "ui.router",
+    "categories",
+    "categories.bookmarks"
+  ])
   .config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
       //abstract state serves as a PLACEHOLDER or NAMESPACE for application states
